b887033da828d56c766bb956de7c0fc4b8824cbd
Barbosa <pbarboje@myuax.com>
2024-10-12 13:01:46 +0200

    FET: Component.java Assil

--- src/main/java/com/example/parcial1assilbarbosa/model/Component.java
+++ src/main/java/com/example/parcial1assilbarbosa/model/Component.java
@@ -1,4 +1,14 @@
 package com.example.parcial1assilbarbosa.model;
 
+
 public class Component {
+    private String type;
+
+    public Component(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
