0569ae40b19457d9a11799b2217bba0c45927ae3
Barbosa <pbarboje@myuax.com>
2024-10-12 13:03:37 +0200

    FET: AssemblyLineService.java

--- src/main/java/com/example/parcial1assilbarbosa/service/AssemblyLineService.java
+++ src/main/java/com/example/parcial1assilbarbosa/service/AssemblyLineService.java
@@ -1,4 +1,25 @@
 package com.example.parcial1assilbarbosa.service;
 
-public class AssemblyLineService {
+
+import com.example.parcial1assilbarbosa.model.Component;
+import java.util.concurrent.BlockingQueue;
+
+public class AssemblyLineService implements Runnable {
+    private final BlockingQueue<Component> buffer;
+
+    public AssemblyLineService(BlockingQueue<Component> buffer) {
+        this.buffer = buffer;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Component component = buffer.take();
+                System.out.println("Assembling: " + component.getType());
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
