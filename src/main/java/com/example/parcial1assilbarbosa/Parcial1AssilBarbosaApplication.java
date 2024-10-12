15261f4667984fe5f8d7601b270abcd0ba366c51
Barbosa <pbarboje@myuax.com>
2024-10-12 12:26:08 +0200

    PP

---
+++ src/main/java/com/example/parcial1assilbarbosa/Parcial1AssilBarbosaApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcial1assilbarbosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial1AssilBarbosaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parcial1AssilBarbosaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parcial1assilbarbosa/Parcial1AssilBarbosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcial1assilbarbosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial1AssilBarbosaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
